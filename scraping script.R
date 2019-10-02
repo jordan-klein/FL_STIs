99569b0bd0eca20615673909fbafa84ba8e2bca8
jordan-klein <jdkny1@gmail.com>
2019-10-02 17:40:33 -0400

    sars scrape

--- scraping script.R
+++ scraping script.R
@@ -9,3 +9,10 @@ read_html("http://www.flhealthcharts.com/charts/OtherIndicators/NonVitalSTDTenYr
   html_nodes("#gvTenYrGrid") %>% 
   html_table() %>% 
   .[[1]] -> inf_syph
+
+read_html("https://www.who.int/csr/sars/country/2003_07_11/en/") %>% 
+  html_nodes(".tableData") %>% 
+  html_table(header = T) %>%
+  .[[1]] -> sars
+
+View(sars)
